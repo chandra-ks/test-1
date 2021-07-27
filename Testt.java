0472a9c5d12d6f59c4a952b8ed30a6e0178e33c0
Chandra <chandrakalidindi5@gmail.com>
2021-07-27 16:59:34 +0530

    removed sub method

--- Testt.java
+++ Testt.java
@@ -2,10 +2,6 @@ int display(){
 System.out.println("Display method..!!");
 }
 
-int sub(){
-System.out.println("Subtract method..!!");
-}
-
 int mul(){
 System.out.println("Mul method..!!");
 }
@@ -14,4 +10,20 @@ int div(){
 System.out.println("Div method..!!");
 }
 
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
   
