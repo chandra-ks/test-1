b7f06bc71ebcac12c6463c3c92c8f8e5e1f332f7
Chandra <chandrakalidindi5@gmail.com>
2021-08-05 14:32:07 +0530

    GUI test 3

---
+++ Test1 - Copy.java
@@ -0,0 +1,35 @@
+int display(){
+System.out.println("Display method..!!");
+}
+
+int mul(){
+System.out.println("Mul method..!!");
+}
+
+System.out.println("Div method..!!");
+System.out.println("Div method..!!");
+
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+  
---
+++ Testt - Copy.java
@@ -0,0 +1,28 @@
+int display(){
+System.out.println("Display method..!!");
+}
+
+int mul(){
+System.out.println("Mul method..!!");
+}
+
+System.out.println("Div method..!!");
+System.out.println("Div method..!!");
+
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+
+System.out.println("Mul method..!!");
+System.out.println("Mul method..!!");
+  
